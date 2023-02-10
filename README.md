d213db2342af576c7725f146f8017cc69e561ce1
Victor Rodrigues <victortavi@outlook.com>
2023-02-10 02:01:05 -0300

    [Fix]
    Com vetor invertido

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Alura Midi Dubstep
+
+Esse projeto, é um fork do Aluramid ainda em desenvolvimento com apoio do curso [JavaScript para Web: Crie páginas dinâmicas](https://cursos.alura.com.br/course/javascript-web-paginas-dinamicas) oferecido pela escola de Tecnologia.
+[Alura](https://www.alura.com.br/)
+
+## Aqui algumas ideias e features futuras
+
+- [ ] Hospedagem no [CodePen](https://codepen.io/)
+- [ ] Trocar entre o [Drumpad](https://www.google.com/search?q=Drum+pads) e Bateria em svg
+- [ ] Um compositor de Batidas
+- [x] Permitir a execução dos sons através das teclas
+- [ ] (Recorder) gravar de sucessão de teclas pressionadas para formar uma música.
+- [ ] Colocar repetição de batidas assim como no Drumpad real
+- [ ] Definir volume nos beats
+- [ ] Permitir carregamento de beats personalizados
+- [ ] Corrigir efeito de click via teclado nos botões
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,76 @@
-document.querySelectorAll('.teclado button').forEach(item => {
-  const audio = document.querySelector(`#som_${item.classList[1]}`);
-  item.addEventListener('click', _ => {
-    audio.currentTime = 0;
-    audio.play();
+(_ => {
+  document.querySelectorAll('.teclado button').forEach(item => {
+    const audio = document.querySelector(`#som_${item.classList[1]}`);
+    item.addEventListener('click', _ => {
+      audio.currentTime = 0;
+      audio.play();
+    });
   });
-});
 
+  const pomSong = document.querySelector('#som_tecla_pom');
+  const clapSong = document.querySelector('#som_tecla_clap');
+  const pshSong = document.querySelector('#som_tecla_psh');
+  const timSong = document.querySelector('#som_tecla_tic');
+
+  const stepsMilliseconds = [610, 500, 150, 150, 500, 600, 0].reverse();
+  const sounds = [
+    pomSong,
+    pomSong,
+    pshSong,
+    pomSong,
+    pomSong,
+    pshSong,
+    clapSong
+  ].reverse();
+
+  let playPause = false;
+  const playButton = document.querySelector('.play');
+  playButton.addEventListener('click', _ => {
+    let beatStep = sounds.length - 1;
+    function step() {
+      setTimeout(_ => {
+        sounds[beatStep].currentTime = 0;
+        sounds[beatStep].play();
+        beatStep--;
+        if (!beatStep) beatStep = sounds.length - 1;
+        if (playPause) step();
+      }, stepsMilliseconds[beatStep]);
+    }
+    playPause = !playPause;
+    if (playPause) step();
+  });
+
+  document.addEventListener('keypress', e => {
+    console.log(e);
+
+    if (e.code == 'KeyQ') {
+      pomSong.currentTime = 0;
+      pomSong.play();
+      return;
+    }
+
+    if (e.code == 'KeyW') {
+      clapSong.currentTime = 0;
+      clapSong.play();
+      return;
+    }
+
+    if (e.code == 'KeyZ') {
+      pshSong.currentTime = 0;
+      pshSong.play();
+      return;
+    }
+  });
+})();
+
+/*
+//musica - 2
 const pomSong = document.querySelector('#som_tecla_pom');
 const clapSong = document.querySelector('#som_tecla_clap');
 const pshSong = document.querySelector('#som_tecla_psh');
-const stepsMilliseconds = [200, 500, 150, 150, 500, 600, 120, 120, 120];
+const stepsMilliseconds = [
+  200, 500, 150, 150, 500, 600, 120, 120, 120, 120
+].reverse();
 const sounds = [
   pomSong,
   pomSong,
@@ -19,20 +80,23 @@ const sounds = [
   clapSong,
   pshSong,
   pshSong,
+  pshSong,
   pshSong
-];
+].reverse();
 
-document.querySelector('#play').addEventListener('click', _ => {
-  let beatStep = 0;
+let playPause = false;
+document.querySelector('.play').addEventListener('click', _ => {
+  let beatStep = sounds.length - 1;
   function step() {
     setTimeout(_ => {
       sounds[beatStep].currentTime = 0;
       sounds[beatStep].play();
-      beatStep++;
-      if (beatStep == stepsMilliseconds.length) beatStep = 0;
-
-      step();
+      beatStep--;
+      if (!beatStep) beatStep = sounds.length - 1;
+      if (playPause) step();
     }, stepsMilliseconds[beatStep]);
   }
-  step();
+  playPause = !playPause;
+  if (playPause) step();
 });
+*/
